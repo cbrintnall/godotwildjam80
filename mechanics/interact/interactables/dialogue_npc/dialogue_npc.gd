873b1fb8fa7cfd0bdd98d0215236d59ec9caf65f
Nathan Straus <natestrausdev@outlook.com>
2025-04-13 23:04:33 -0700

    load after merge

--- mechanics/interact/interactables/dialogue_npc/dialogue_npc.gd
+++ mechanics/interact/interactables/dialogue_npc/dialogue_npc.gd
@@ -3,7 +3,7 @@ extends Interactable
 @export var dialogue_timeline_name = ""
 
 func _on_interacted(_body):
-    if Dialogic.current_timeline != null:
-        return
-        
-    Dialogic.start(dialogue_timeline_name)
+	if Dialogic.current_timeline != null:
+		return
+		
+	Dialogic.start(dialogue_timeline_name)
