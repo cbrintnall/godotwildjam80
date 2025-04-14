38c193fc4ee23340ef215587eab0900dffa4140c
Nathan Straus <natestrausdev@outlook.com>
2025-04-13 22:59:23 -0700

    dialogue

---
+++ mechanics/interact/interact_ray.gd
@@ -0,0 +1,29 @@
+extends RayCast3D
+
+@onready var prompt = $Control/Prompt
+@onready var cursor_icon = $Control/CursorIcon
+
+var icons = { 
+	"speak": preload("res://mechanics/interact/speak-icon.png") 
+}
+
+func _physics_process(delta: float) -> void:
+	prompt.text = ""
+	cursor_icon.texture = null
+	
+	if is_colliding():
+		var collider = get_collider()
+		
+		if collider is Interactable:
+			_set_icon_by_name(collider.icon_name)
+			prompt.text = collider.prompt
+			
+			if Input.is_action_just_pressed("interact"):
+				collider.interact(owner)
+			
+func _set_icon_by_name(name: String):
+	if name in icons.keys():
+		cursor_icon.texture = icons[name]
+	else:
+		cursor_icon.texture = null
+			
--- scenes/test_area.gd
+++ scenes/test_area.gd
@@ -5,3 +5,7 @@ extends Node3D
 func _physics_process(_delta: float) -> void:
 	get_tree().call_group("Enemies", "update_target_location", player.global_transform.origin)
 	
+
+
+func _on_ready() -> void:
+	pass # Replace with function body.
